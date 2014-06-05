5eaab3899b9533bfccfe541d1dc1fcdf4b0e0ec3
Racheet <racheet@gmail.com>
2014-06-05 18:15:35 +0000

    messy but it works

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-/**
- * Created with Empire-Gazeteer-Crawler.
- * User: Racheet
- * Date: 2014-06-02
- * Time: 04:10 PM
- * To change this template use Tools | Templates.
- */
 var phantom = require("phantom"),
-    fs = require("fs");
+    fs = require("fs"),
+    EventEmitter = require("events").EventEmitter,
+    util = require("util");
 
 var output = fs.createWriteStream("output/data.json", {
     "flags": "w",
@@ -14,21 +9,66 @@ var output = fs.createWriteStream("output/data.json", {
 });
 
 var data = {};
+var Browser = function() {
+    var self = this;
+    EventEmitter.call(this);
+    
+    this.createPage = function () {};
+    
+    phantom.create(function (instance) {
+        self.pageCreator = instance;
+        self.createPage = instance.createPage;
+        self.emit("readyToCrawl");
+        console.log("Log: Event Fired");
+    });   
+    
+    this.emit("init");
+    
+    return this;
+};
 
-phantom.create(function (instance) {
-    instance.createPage (function(page) {
+util.inherits(Browser,EventEmitter);
+
+var browser = new Browser();
+
+browser.on("readyToCrawl", function() {
+    browser.pageCreator.createPage(function(page) {
         page.open("https://pisaca-jelick.codio.io/Provinces/Gazetteer.htm", function(status){
             console.log("opened page: Gazeteer?", status);
             page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js", function(result) {
                  page.evaluate(function(){
                     $("#testdiv").html("Jquery Present");
                     return document.getElementById("testdiv").innerHTML;
-                     
+
                 }, function(result) {
                     console.log("Page h1:", result);
-                    instance.exit();
+                    browser.pageCreator.exit();
                 });   
             });
         });
-    });
+    });   
 });
+    
+console.log("Log: Event Handler Attached")
+
+function crawlPage (page,url) {
+    if(typeof url != "string") throw "url needs to be a string";
+    if(url.substring(0,5) !== "http") throw "second argument needs to be a url";
+    
+    page.open(url, function(status) {
+        if(status !== "success") throw status;
+        console.log("Opened Page:",url);
+        
+        page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js", function (result) {
+            page.evaluate(function(){
+                //payload goes here
+                return $("title").html();          
+                
+            }), function (result) {
+                console.log("result")
+                page.close();
+            }
+            
+        });
+    })
+}
