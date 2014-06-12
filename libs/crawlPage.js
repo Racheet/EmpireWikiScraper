17b44d63983cc2d9998d6381daa8d9b44f777d12
Racheet <racheet@gmail.com>
2014-06-12 11:50:40 +0000

    Error objects are big and clever

--- libs/crawlPage.js
+++ libs/crawlPage.js
@@ -1,10 +1,10 @@
 function crawlPage (browser,page,url,payload,localDataBind) {
     "use strict";
-    if(typeof url != "string") {throw "url needs to be a string"; }
-    if(url.match(/^https*:\/\//) === null) {throw "second argument needs to be a url"; }
+    if(typeof url != "string") {throw new Error("url needs to be a string"); }
+    if(url.match(/^https*:\/\//) === null) {throw new Error("third argument needs to be a url"); }
     
     page.open(url, function(status) {
-        if(status !== "success") throw status;
+        if(status !== "success") throw new Error(status);
         console.log("Opened Page:",url);
         
         page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js", function (result) {
