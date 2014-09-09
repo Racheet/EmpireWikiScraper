5c93b1d5aa0e0ab2715e6f96cfb41c6596a283a4
Racheet <racheet@gmail.com>
2014-09-09 11:25:17 +0000

    added automatic nation matching

--- data-enricher/index.js
+++ data-enricher/index.js
@@ -1,49 +1,93 @@
 #!/usr/local/bin/node
+
 var prompt = require("prompt"),
     fs = require("fs"),
     async = require("async");
 
 
 var inputPath = process.argv[2] || __dirname + "/input/data.json";
-
-
-var data = JSON.parse(fs.readFileSync(inputPath).toString());
+var data = JSON.parse(fs.readFileSync(inputPath)
+    .toString());
+var nations = JSON.parse(fs.readFileSync(__dirname + "/static/nations.json"));
 
 prompt.start();
 
 
 function getTerritoryData(territory, callback) {
+    //setup
+    function matchTerritoryToNation(territory) {
+        territory.nation = findNationOf(territory.name);
+    }
 
-    var schema = {
-        "nation": {
-            "nation": "What Nation does this territory belong to?",
-            "type": "string",
-            "required": true
+    function checkIfNationIsCorrect(callback) {
+        var schema = {
+            name: "nation",
+            message: "Is " + territory.name + " in " + territory.nation + "?" + "(yes/no)",
+            validator: /^[yY]$|^[yY]es$|^[nN]$|^[nN]o$/,
+            warning: 'Please respond yes or no'
+
+        };
+
+        prompt.get(schema, function (err, result) {
+            if(err) return callback(err);
+            if(result.nation.toLowerCase()
+                .match(/^[yY]$|^[yY]es$/)) {
+                callback(true);
+            } else {
+                callback(false);
+            }
+        });
+    }
+
+    function manuallyUpdateNation(callback) {
+        
+        console.log("crumbs!");
+        callback();
+    }
+
+    //logic
+    console.log("This Territory is:", territory.name);
+    matchTerritoryToNation(territory);
+
+    checkIfNationIsCorrect(function (nationIsCorrect) {
+        if(nationIsCorrect) {
+            return callback(null, territory);
+        } else {
+            manuallyUpdateNation(function () {
+                return callback(null, territory);
+            });
         }
-    };
-    
-    console.log("This Territory is:",territory.name);
-    prompt.get(["nation"], function (err, result) {
-        if(err) return callback(err);
-        territory.nation = result.nation;
-        if (callback) return callback(null, territory);
     });
+
 }
 
-function writeOutProvincesJson (err, provinces) {
-    if (err) return console.error(err);
-    
+
+function writeOutProvincesJson(err, provinces) {
+    if(err) return console.error(err);
+
     var output = fs.createWriteStream(__dirname + "/output/data.json", {
         "flags": "w",
         "encoding": "utf8"
     });
-    
-    output.end(provinces, "utf8", function() {
-       console.log("log: output file written");
-   });
+
+    output.end(provinces, "utf8", function () {
+        console.log("log: output file written");
+    });
 }
 
-async.mapSeries(data,getTerritoryData,function(err,results){
-    if (err) throw new Error(err);
-    writeOutProvincesJson(null,JSON.stringify(results));
+
+function findNationOf(territory) {
+    var name = null;
+    nations.forEach(function (nation) {
+        if(nation.territories.indexOf(territory) != -1) {
+            name = nation.name;
+        }
+    });
+    return name;
+}
+
+
+async.mapSeries(data, getTerritoryData, function (err, results) {
+    if(err) throw new Error(err);
+    writeOutProvincesJson(null, JSON.stringify(results));
 });
